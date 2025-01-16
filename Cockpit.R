d3be8be3ef30793ca99d6273e7962081b2eca526
Daniel Podgorski <podgorski.dan@gmail.com>
2025-01-16 21:21:34 +0100

    changed structures of funcs

--- Cockpit.R
+++ Cockpit.R
@@ -2,13 +2,11 @@ library("usethis")
 library("gitcreds")
 
 
-generate_numbers = function(n) {
-  sample(1:100, n)
-}
 
 main=function(){
   
-  #browser()
+  browser()
+  
   
   lucky_numbers <- generate_numbers(5)
   
@@ -16,8 +14,29 @@ main=function(){
   
   starting_vector <- c(2,4,5)
   
+  my_string <- "hello"
+  
+  my_string <- extract_character_value(my_string,1,3)
+  
+  
+  
   
 }
+
+generate_numbers = function(n) {
+  sample(1:100, n)
+}
+
+
+
+extract_character_value <- function(my_string,start,end){
+
+  my_string <- substr(my_string,start,end)
+  
+  return(my_string)
+  
+}
+
 
 
 main()
